c859f1969be355f9bd753acb4213fdd0ffab3a24
Luana Luz <luanarahal@gmail.com>
2023-08-27 22:38:54 -0300

    feat: finishing exercises of date

--- revisoes/revisao-3.js
+++ revisoes/revisao-3.js
@@ -62,3 +62,77 @@ const difference = (valueA, valueB) => {
 };
 
 console.log(difference(dateA, dateB));
+
+//Escreva uma função que aceite um ano como entrada e retorne true se for um ano bissexto ou false se não for.
+
+const leapYear = (year) => {
+  if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
+    return true
+  } else {
+    return false
+  } 
+}
+
+console.log(leapYear(1996));
+
+//Crie uma função que aceite uma data e um número de dias como entrada. A função deve adicionar o número de dias à data e retornar a nova data.
+const dateD = new Date(2023, 5, 20);
+
+const addDaysToDate = (date, days) => {
+  return new Date(date.setDate(date.getDate() + days));
+}
+
+console.log(addDaysToDate(dateD, 1))
+
+//Escreva uma função que aceite uma data e retorne a data no formato "DD de [mês por extenso] de AAAA". Por exemplo, "17 de Agosto de 2023".
+const date = new Date();
+
+  const MONTHS_IN_PTBR = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+  ];
+
+  const WEEK_DAYS = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+  ];
+
+const formatDate = (date) => {
+  const day = padStart(date.getDate());
+  const month = MONTHS_IN_PTBR[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} de ${month} de ${year}`
+}
+
+console.log(formatDate(date));
+
+//Peça ao usuário que insira uma data e exiba informações como o dia da semana, o mês, o ano, etc.
+const valueC = new Date();
+
+const showDateDetails = (date) => {
+  const day = padStart(date.getDate());
+  const weekDay = WEEK_DAYS[date.getDay()];
+  const month = MONTHS_IN_PTBR[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `Dia: ${day} | Mês: ${month} | Ano: ${year} | Dia da semana: ${weekDay}`; 
+}
+
+console.log(showDateDetails(valueC))
+
